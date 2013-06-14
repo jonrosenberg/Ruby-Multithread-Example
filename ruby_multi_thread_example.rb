dc48545096dd62a7e8efaa1f7627b60192e976f9
Jonathan Rosenberg <jon@jackrabbitmobile.com>
2013-06-13 19:44:30 -0500

    Update script with comments and out of bounds fix

--- ruby_multi_thread_example.rb
+++ ruby_multi_thread_example.rb
@@ -4,7 +4,9 @@ require 'thread'
 LENGTH = 1000
 
 puts "multi-thread example"
+@mutex = Mutex.new
 
+#Create array of size 1000 and initiate the values to zero
 @ary = [0]
 @ary *= LENGTH
 
@@ -12,50 +14,46 @@ puts "multi-thread example"
 @t1_count = 0
 @t2_count = 0
 
-mutex = Mutex.new
-cv = ConditionVariable.new
-
-def inc
-	#cv.wait(mutex)
-	@ary[@i] += 1
-	@i += 1
-	#cv.signal
+# Increment the next element in the array
+def increment
+	@mutex.lock 		# Lock code block
+		@ary[@i] += 1
+		@i += 1
+	@mutex.unlock 		# Unlock code block
+	sleep(0.000002)		# Put thread to sleep briefly to prove multi-thread functionality
 end
 
-
+# Run thread 1
 puts "Started At #{Time.now}"
-t1=Thread.new{
-	puts "t1 at: #{Time.now}"
+t1=Thread.new do
 	begin
-		mutex.synchronize do
-			inc
+		if(@i < LENGTH)			# Protect from array out of bounds
+			increment()
+			@t1_count += 1 		# Count number of times thread 1 incremented array
 		end
-		@t1_count += 1
-		sleep(0.00055)
-
 	end while @i < LENGTH
-	puts "t1 at: #{Time.now}"
-}
+	puts "t1 ends at: #{Time.now}"
+end
 
-t2=Thread.new{
-	puts "t2 at: #{Time.now}"
+# Run thread 2
+t2=Thread.new do
 	begin
-		mutex.synchronize do
-			inc()
+		if(@i < LENGTH)			# Protect from array out of bounds
+			increment()
+			@t2_count += 1 		# Count number of times thread 2 incremented array
 		end
-		@t2_count += 1
-		sleep(0.0005)
 	end while @i < LENGTH
-	puts "t2 at: #{Time.now}"
-}
+	puts "t2 ends at: #{Time.now}"
+end
+
 t1.join
 t2.join
 puts "End at #{Time.now}"
 
 puts "ary :  #{@ary}"
 puts "total :  #{@i}"
-puts "t1_count :  #{@t1_count}"
-puts "t2_count :  #{@t2_count}"
+puts "thread 1 increment count :  #{@t1_count}"
+puts "thread 2 increment count :  #{@t2_count}"
 
-mutex.lock
+
 
